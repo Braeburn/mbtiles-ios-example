cf2a655a900e783c61e284783a1ba4654b80af73
Justin R. Miller <incanus@codesorcery.net>
2011-03-07 16:21:36 -0500

    updated copyrights & cleaned out some of Apple's boilerplate

--- Classes/MBTiles_ExampleAppDelegate.h
+++ Classes/MBTiles_ExampleAppDelegate.h
@@ -3,7 +3,7 @@
 //  MBTiles Example
 //
 //  Created by Justin R. Miller on 3/7/11.
-//  Copyright 2011 Code Sorcery Workshop. All rights reserved.
+//  Copyright 2011 Development Seed. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
